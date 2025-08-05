4e1ef16c2e36ec8f8f8c7ebb7678e3c6a102460d
ANUSHKA-ROY05 <roy916968@gmail.com>
2025-08-05 09:58:25 +0000

    changed name of 1st file as 01_variables

---
+++ 01_Basics/01variables.js
@@ -0,0 +1,10 @@
+const v1 = 4444;
+let v2 = "anu"
+/* we generally don't use var method
+ISSUE IN BLOCK SCOPE ...or....FUNCTIONAL SCOPE*/
+var v3 = " is "
+v4 = " cute"
+let v5;
+console.log(v1)
+console.log(v5); console.log("variable with nothing in it")
+console.table([v1,v2,v3,v4,v5])
