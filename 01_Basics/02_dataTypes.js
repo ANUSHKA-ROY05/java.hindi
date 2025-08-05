964c00eb47bf1f95bbb00da4bbba1a62f6ebe928
ANUSHKA-ROY05 <roy916968@gmail.com>
2025-08-05 09:57:46 +0000

    About datatypes and their notes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"  ;  //this is used to tell pc that we will be using all standard syntax/keywords of modern js
+//due to change in format of various things and addition of new concepts like modules , class , security etc ...the org made a standard java language.
+// alert(3+3) //this would give error becuz we are using node js not browser
+
+
+//Datatypes
+let name = "anushka"    //string
+let age = 18            // number
+let isLOggedin = false  //Boolean
+
+/*
+ ranges of primituve datatypes in js
+ number => 2to the power 53
+ bigint
+ string => ""
+ boolean => true/false
+ null => standalone value , also a datatype in js,also a representation of empty value
+ undefined => iska mtlb abhi value assign nahi hui hai
+ symbol=> mostly usi=ed in react , used to give uniqueness to cmponents
+
+ object
+ */
+console.log(typeof"anushka")
+console.log(typeof age)
+console.log(typeof null)    //it is of type : object
+console.log(typeof undefined) //it is of type undefined
