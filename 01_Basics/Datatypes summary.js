5470967590a84ca7d7236dc1adc1f034fb906623
ANUSHKA-ROY05 <roy916968@gmail.com>
2025-08-05 18:15:15 +0000

    Datatype Summarya

---
+++ 01_Basics/Datatypes summary.js
@@ -0,0 +1,20 @@
+/* JavaScript is a dynamically typed language
+In dynamically typed languages, type checking occurs at runtime,
+ meaning the data type of a variable is determined during program execution,
+ not at compile time*/
+//primitive
+
+/*7 Categories:
+string 
+Number
+Boolean
+Null
+Undefined
+symbol
+BigInt
+*/
+
+// Non Primitive ...or Refrence type
+/*Array
+Objects
+Functions */
